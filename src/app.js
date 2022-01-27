559cd716bb8d4e68f8a300d46ca91a787f2e5a25
Maxime <maxime.daraize@gmail.com>
2022-01-27 12:46:18 -0500

    change function for arrow function + export it

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-function srcsm(string) {
+export const srcsm = (string) => {
   return string
     .split("")
     .map((item, index) =>
       index % 2 === 0 ? item.toUpperCase() : item.toLowerCase()
     )
     .join("");
-}
+};
